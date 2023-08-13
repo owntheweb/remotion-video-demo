e3df02763b7069a1737504b280730216cc738210
owntheweb <67289492+owntheweb@users.noreply.github.com>
2023-08-13 08:47:18 -0600

    animated remotion logo cleanup

--- src/sequence/ReactAndRemotion/ReactAndRemotion.tsx
+++ src/sequence/ReactAndRemotion/ReactAndRemotion.tsx
@@ -1,23 +1,9 @@
 'use strict';
 import React from 'react';
-import {AbsoluteFill, Easing, interpolate, useCurrentFrame} from 'remotion';
+import {AbsoluteFill} from 'remotion';
 import {AnimatedLogo} from './remotionLogo/AnimatedLogo';
 
 export const ReactAndRemotion: React.FC = () => {
-	const frame = useCurrentFrame();
-
-	/*
-	// Sidebar animation
-	const iconBarOpacity = interpolate(frame, [20, 40], [0, 0.25], {
-		extrapolateLeft: 'clamp',
-		extrapolateRight: 'clamp',
-	});
-	const iconBarX = interpolate(frame, [20, 40], [-50, 0], {
-		extrapolateLeft: 'clamp',
-		extrapolateRight: 'clamp',
-	});
-	*/
-
 	return (
 		<AbsoluteFill className="bg-slate-900 text-white text-7xl p-24">
 			<AnimatedLogo theme="dark" />
